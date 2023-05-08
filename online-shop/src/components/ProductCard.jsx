2c5a54d51104a6cb4f0807bbe06d8495fd514ba5
AdrianLadia <ladia.adrian@gmail.com>
2023-05-09 06:30:59 +0800

    Revert "."
    This reverts commit 0475dc0e9f78ecbaf60b0fdb7e2c3f86beeeb7a7.

--- online-shop/src/components/ProductCard.jsx
+++ online-shop/src/components/ProductCard.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Paper from '@mui/material/Paper';
 import { Button, Typography } from '@mui/material';
 import { Snackbar } from '@material-ui/core';
-import { useState, useContext, useEffect, useRef } from 'react';
+import { useState, useContext, useEffect,useRef } from 'react';
 import UseWindowDimensions from './UseWindowDimensions';
 import TextField from '@mui/material/TextField';
 import ProductCardModal from './ProductCardModal';
@@ -13,9 +13,9 @@ import { ThemeProvider } from '@mui/material/styles';
 import theme from '../colorPalette/MaterialUITheme';
 import { BsFillInfoCircleFill } from 'react-icons/bs';
 import { FaHandPointDown } from 'react-icons/fa';
-import classNames from 'classnames';
 
 const ProductCard = (props) => {
+
   // console.log(props)
   const [quantity, setQuantity] = useState('');
   const [open, setOpen] = useState(false);
@@ -29,14 +29,14 @@ const ProductCard = (props) => {
   const { cart } = useContext(AppContext);
   const [iconVisible, setIconVisible] = useState(true);
   const ref = useRef(null);
-  const showTutorial = props.showTutorial;
-  const setShakeCartAnimation = props.setShakeCartAnimation;
-  const retailStocksAvailable = props.stocksAvailable;
-  const retailAverageSalesPerDay = props.averageSalesPerDay;
+  const showTutorial = props.showTutorial
+  const setShakeCartAnimation = props.setShakeCartAnimation
+  const retailStocksAvailable = props.stocksAvailable
+  const retailAverageSalesPerDay = props.averageSalesPerDay
 
   // console.log(width)
-
-  let safetyStock;
+  
+  let safetyStock 
   if (product.averageSalesPerDay != undefined) {
     safetyStock = calculations.getSafetyStock(product.averageSalesPerDay);
   }
@@ -44,6 +44,8 @@ const ProductCard = (props) => {
     safetyStock = calculations.getSafetyStock(retailAverageSalesPerDay);
   }
 
+  
+  
   function ClearForm() {
     document.getElementById('inputquantity' + props.product.itemName).value = '';
   }
@@ -84,7 +86,7 @@ const ProductCard = (props) => {
       //back to 0
       setQuantity('');
       //shake cart
-      setShakeCartAnimation(true);
+      setShakeCartAnimation(true)
     }
   }
 
@@ -121,9 +123,9 @@ const ProductCard = (props) => {
   function responsiveStyle() {
     if (width < 396) {
       return '19px';
-    } else if (width < 1) {
-      return;
-    } else {
+    }else if(width < 1){
+      return 
+    }else{
       return '23px';
     }
   }
@@ -131,7 +133,7 @@ const ProductCard = (props) => {
   function responsivePrice() {
     if (width < 300) {
       return '13px';
-    } else if (width < 768) {
+    }else if (width < 768) {
       return '13px';
     } else if (width < 1024) {
       return '13px';
@@ -171,39 +173,32 @@ const ProductCard = (props) => {
     return () => clearTimeout(timer);
   }, []);
 
+
   function responsiveImgWidth() {
     return 'max-w-xl';
-  }
+} 
 
-  function responsiveWidth() {
-    return 'max-w-lg';
-  }
+function responsiveWidth() {
+  return 'max-w-lg';
+} 
 
   return (
     <ThemeProvider theme={theme}>
-      <div className={classNames('flex', 'justify-center', 'h-80', 'w-11/12', '2xs:w-96', responsiveWidth())}>
+      <div className={" flex justify-center h-80 w-11/12 2xs:w-96"+ responsiveWidth()}>
+
+      {/* <div className="flex justify-center h-80 w-full 2xs:w-96 2xs:max-w-lg"> */}
         <Paper
           elevation={10}
-          className={classNames(
-            'flex',
-            'flex-row',
-            'rounded-4xl',
-            'w-11/12',
-            'justify-center',
-            'my-5',
-            'bg-gradient-to-r',
-            'from-color60',
-            'via-color60',
-            'to-color10c',
-            'shadow-lg',
-            'shadow-slate-500'
-          )}
+          // className="flex flex-row w-11/12 justify-center my-5 h-60 bg-color30 "
+          className="flex flex-row rounded-4xl w-11/12 justify-center my-5 
+                      bg-gradient-to-r from-color60 via-color60 to-color10c shadow-lg shadow-slate-500"
+          // className="flex flex-row rounded-4xl w-11/12 justify-center my-5 bg-gradient-to-r from-color60 to-color10c drop-shadow-2xl"
         >
           {/* IMAGE */}
-          <div className="w-8/12 relative rounded-4xl cursor-pointer">
+          <div className=" w-8/12 relative rounded-4xl cursor-pointer" >
             <div className="absolute inset-0 flex justify-center mt-12 p-2" onClick={() => setModal(true)}>
-              {iconVisible && showTutorial && (
-                <div className="">
+              {(iconVisible && showTutorial) ? (
+                <div className=''>
                   <FaHandPointDown
                     onClick={() => setModal(true)}
                     color="#6bd0ff"
@@ -211,21 +206,26 @@ const ProductCard = (props) => {
                     className="animate-bounce-fade-5 mt-4"
                   />
                   <h1 className="absolute text-sm -top-20 left-14 whitespace-nowrap">
-                    <span className="text-transparent font-bold bg-clip-text bg-color10b">TAP FOR MORE INFO</span>
+                    <span className="text-transparent font-bold bg-clip-text bg-color10b"> 
+                    TAP FOR MORE INFO
+                    </span>
                   </h1>
                 </div>
-              )}
+              ) : null}
             </div>
-            <img
-              src={props.product.imageLinks[0]}
-              alt={props.product.itemName}
-              className={classNames('h-full', 'object-cover', 'rounded-4xl', 'border-color60')}
-              onClick={() => setModal(true)}
-            />
+              <img
+                  src={props.product.imageLinks[0]}
+                  alt={props.product.itemName}
+                  // className={"h-full object-cover saturate-150" + responsiveImgWidth()}
+                  // className={"h-full object-cover rounded-r-4xl border-t-2 border-b-2"}
+                  className={"h-full object-cover rounded-4xl  border-color60 "}
+                  onClick={() => setModal(true)}
+              >
+              </img>
           </div>
           {/* IMAGE */}
           {/* DETAILS */}
-          <div className="flex flex-col ml-3 w-5/12 m-1">
+          <div className="flex flex-col ml-3 w-5/12 m-1 ">
             {outofstock === true ? (
               <div className="flex flex-row h-1/12">
                 <Typography fontSize={responsiveFont()} color="red">
@@ -235,39 +235,48 @@ const ProductCard = (props) => {
               </div>
             ) : (
               <>
-                {lowstock === true && product.unit !== 'Pack' && (
+                {lowstock === true && product.unit != 'Pack' ? (
                   <div className="flex flex-row h-1/12">
                     <Typography fontSize={responsiveFont()} color="red">
                       Stocks left
                     </Typography>
-                    <span className="flex h-3 w-3 ml-1">
+                    <span className="flex h-3 w-3 ml-1 ">
                       <span className="inline-flex items-center justify-center mt-0 2xl:mt-1 py-2 px-1 text-xs font-semibold text-white bg-red-600 rounded-full">
                         {props.product.stocksAvailable - safetyStock}
                       </span>
                     </span>
                   </div>
+                ) : (
+                  <div className="h-1/12"> </div>
                 )}
               </>
             )}
 
-            <div className="h-2/6">
-              <Typography sx={{ fontSize: responsiveStyle(), mr: 1, cursor: 'help' }} onClick={() => setModal(true)}>
+            {/* {console.log(props.product)} */}
+            <div className="h-2/6 ">
+              <Typography
+                sx={{ fontSize: responsiveStyle(), mr: 1, cursor: 'help' }}
+                onClick={() => setModal(true)}
+              >
                 {props.product.itemName}
               </Typography>
             </div>
 
-            <div className="h-1/9 w-max mt-2 2xs:mt-0">
+            <div className="h-1/9 w-max mt-2 2xs:mt-0 ">
               <Typography
-                sx={{ fontSize: responsivePrice(), mt: 2, cursor: 'help' }}
-                className="tracking-tight"
+                sx={{ fontSize: responsivePrice(),mt: 2 , cursor: 'help' }}
+                className='tracking-tight'
                 onClick={() => setModal(true)}
               >
                 Pieces: {props.product.pieces}
               </Typography>
             </div>
 
-            <div className="h-1/6     flex items-center">
-              <Typography sx={{ fontSize: responsivePrice(), mb: 1, cursor: 'text' }} className="tracking-tight">
+            <div className="h-1/6 flex items-center">
+              <Typography 
+                sx={{ fontSize: responsivePrice(), mb: 1, cursor: 'text' }}
+                className='tracking-tight'
+              >
                 Price: {'₱ ' + props.product.price}
               </Typography>
             </div>
@@ -275,7 +284,8 @@ const ProductCard = (props) => {
             <div className="flex flex-row items-center">
               <button
                 id="addtocartbutton"
-                className="h-max w-5/12 2xs:w-5/12 py-3 2xs:px-2 rounded-lg text-xs text-black border-2 border-color60 bg-color10c hover:bg-color1"
+                className=" h-max w-5/12 2xs:w-5/12 py-3 2xs:px-2 rounded-lg text-xs text-black border-2 border-color60 bg-color10c hover:bg-color1"
+                // className=" h-max w-5/12 2xs:w-1/3 py-3 2xs:px-2 rounded text-xs text-black border-2 border-color10a bg-color10a hover:bg-color10c hover:border-4 hover:border-double"
                 type="button"
                 onClick={AddToCart}
               >
@@ -286,216 +296,54 @@ const ProductCard = (props) => {
                 type="number"
                 color="enter"
                 value={quantity}
-                onChange={(event) => {
-                  setQuantity(event.target.value);
-                }}
-                className="m-2 h-max w-7/12 rounded-xl"
+                onChange={(event) => {setQuantity(event.target.value);}}
+                className="m-2 h-max w-7/12 rounded-xl "
                 label="Qty."
                 InputLabelProps={{
                   style: {
                     color: '#6ab15d',
-                    fontSize: 15,
-                  },
-                }}
-                sx={{
-                  backgroundColor: '#ffffff',
-                  '& .MuiOutlinedInput-notchedOutline': {
-                    border: 2,
-                    color: '#6ab15d',
-                    borderRadius: 2,
-                  },
-                  '& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline': {
-                    color: '#6ab15d',
-                    border: 2,
-                  },
-                }}
+                    fontSize:15
+                  },}}
+                sx={{backgroundColor:"#ffffff",
+                    '& .MuiOutlinedInput-notchedOutline': {
+                      border: 2 ,
+                      color:'#6ab15d',
+                      borderRadius:2,
+                    },
+                    '& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline': {
+                      color: '#6ab15d',
+                      border:2
+                    }
+                  }}
               />
-            </div>
           </div>
-        </Paper>
-        <div>
-          <ProductCardModal modal={modal} closeModal={closeModal} product={product} />
         </div>
-        <div>
-          <Snackbar
-            className="mb-5 lg:mb-5"
-            variant="success"
-            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-            open={open}
-            onClose={handleClose}
-            message={DisplayItem()}
-            action={
-              <Button color="success" size="small" onClick={handleClose}>
-                {' '}
-                Close{' '}
-              </Button>
-            }
-          />
-        </div>
+      </Paper>
+      <div>
+        <ProductCardModal
+          modal={modal}
+          closeModal={closeModal}
+          product={product}
+        />
       </div>
+      <div>
+        <Snackbar
+          className="mb-5 lg:mb-5"
+          variant="success"
+          anchorOrigin={{ vertical: "top", horizontal: "center" }}
+          open={open}
+          onClose={handleClose}
+          message={DisplayItem()}
+          action={
+            <Button color="success" size="small" onClick={handleClose}>
+              {" "}
+              Close{" "}
+            </Button>
+          }
+        />
+      </div>
+    </div>
     </ThemeProvider>
-
-    // <ThemeProvider theme={theme}>
-    //   <div className={" flex justify-center h-80 w-11/12 2xs:w-96"+ responsiveWidth()}>
-
-    //   {/* <div className="flex justify-center h-80 w-full 2xs:w-96 2xs:max-w-lg"> */}
-    //     <Paper
-    //       elevation={10}
-    //       // className="flex flex-row w-11/12 justify-center my-5 h-60 bg-color30 "
-    //       className="flex flex-row rounded-4xl w-11/12 justify-center my-5
-    //                   bg-gradient-to-r from-color60 via-color60 to-color10c shadow-lg shadow-slate-500"
-    //       // className="flex flex-row rounded-4xl w-11/12 justify-center my-5 bg-gradient-to-r from-color60 to-color10c drop-shadow-2xl"
-    //     >
-    //       {/* IMAGE */}
-    //       <div className=" w-8/12 relative rounded-4xl cursor-pointer" >
-    //         <div className="absolute inset-0 flex justify-center mt-12 p-2" onClick={() => setModal(true)}>
-    //           {(iconVisible && showTutorial) ? (
-    //             <div className=''>
-    //               <FaHandPointDown
-    //                 onClick={() => setModal(true)}
-    //                 color="#6bd0ff"
-    //                 size={40}
-    //                 className="animate-bounce-fade-5 mt-4"
-    //               />
-    //               <h1 className="absolute text-sm -top-20 left-14 whitespace-nowrap">
-    //                 <span className="text-transparent font-bold bg-clip-text bg-color10b">
-    //                 TAP FOR MORE INFO
-    //                 </span>
-    //               </h1>
-    //             </div>
-    //           ) : null}
-    //         </div>
-    //           <img
-    //               src={props.product.imageLinks[0]}
-    //               alt={props.product.itemName}
-    //               // className={"h-full object-cover saturate-150" + responsiveImgWidth()}
-    //               // className={"h-full object-cover rounded-r-4xl border-t-2 border-b-2"}
-    //               className={"h-full object-cover rounded-4xl  border-color60 "}
-    //               onClick={() => setModal(true)}
-    //           >
-    //           </img>
-    //       </div>
-    //       {/* IMAGE */}
-    //       {/* DETAILS */}
-    //       <div className="flex flex-col ml-3 w-5/12 m-1 ">
-    //         {outofstock === true ? (
-    //           <div className="flex flex-row h-1/12">
-    //             <Typography fontSize={responsiveFont()} color="red">
-    //               Out of Stock
-    //             </Typography>
-    //             <span className="flex h-3 w-3 mt-1 ml-2 flex-grow-0"></span>
-    //           </div>
-    //         ) : (
-    //           <>
-    //             {lowstock === true && product.unit != 'Pack' ? (
-    //               <div className="flex flex-row h-1/12">
-    //                 <Typography fontSize={responsiveFont()} color="red">
-    //                   Stocks left
-    //                 </Typography>
-    //                 <span className="flex h-3 w-3 ml-1 ">
-    //                   <span className="inline-flex items-center justify-center mt-0 2xl:mt-1 py-2 px-1 text-xs font-semibold text-white bg-red-600 rounded-full">
-    //                     {props.product.stocksAvailable - safetyStock}
-    //                   </span>
-    //                 </span>
-    //               </div>
-    //             ) : (
-    //               <div className="h-1/12"> </div>
-    //             )}
-    //           </>
-    //         )}
-
-    //         {/* {console.log(props.product)} */}
-    //         <div className="h-2/6 ">
-    //           <Typography
-    //             sx={{ fontSize: responsiveStyle(), mr: 1, cursor: 'help' }}
-    //             onClick={() => setModal(true)}
-    //           >
-    //             {props.product.itemName}
-    //           </Typography>
-    //         </div>
-
-    //         <div className="h-1/9 w-max mt-2 2xs:mt-0 ">
-    //           <Typography
-    //             sx={{ fontSize: responsivePrice(),mt: 2 , cursor: 'help' }}
-    //             className='tracking-tight'
-    //             onClick={() => setModal(true)}
-    //           >
-    //             Pieces: {props.product.pieces}
-    //           </Typography>
-    //         </div>
-
-    //         <div className="h-1/6 flex items-center">
-    //           <Typography
-    //             sx={{ fontSize: responsivePrice(), mb: 1, cursor: 'text' }}
-    //             className='tracking-tight'
-    //           >
-    //             Price: {'₱ ' + props.product.price}
-    //           </Typography>
-    //         </div>
-
-    //         <div className="flex flex-row items-center">
-    //           <button
-    //             id="addtocartbutton"
-    //             className=" h-max w-5/12 2xs:w-5/12 py-3 2xs:px-2 rounded-lg text-xs text-black border-2 border-color60 bg-color10c hover:bg-color1"
-    //             // className=" h-max w-5/12 2xs:w-1/3 py-3 2xs:px-2 rounded text-xs text-black border-2 border-color10a bg-color10a hover:bg-color10c hover:border-4 hover:border-double"
-    //             type="button"
-    //             onClick={AddToCart}
-    //           >
-    //             Add
-    //           </button>
-    //           <TextField
-    //             id="entryquantity"
-    //             type="number"
-    //             color="enter"
-    //             value={quantity}
-    //             onChange={(event) => {setQuantity(event.target.value);}}
-    //             className="m-2 h-max w-7/12 rounded-xl "
-    //             label="Qty."
-    //             InputLabelProps={{
-    //               style: {
-    //                 color: '#6ab15d',
-    //                 fontSize:15
-    //               },}}
-    //             sx={{backgroundColor:"#ffffff",
-    //                 '& .MuiOutlinedInput-notchedOutline': {
-    //                   border: 2 ,
-    //                   color:'#6ab15d',
-    //                   borderRadius:2,
-    //                 },
-    //                 '& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline': {
-    //                   color: '#6ab15d',
-    //                   border:2
-    //                 }
-    //               }}
-    //           />
-    //       </div>
-    //     </div>
-    //   </Paper>
-    //   <div>
-    //     <ProductCardModal
-    //       modal={modal}
-    //       closeModal={closeModal}
-    //       product={product}
-    //     />
-    //   </div>
-    //   <div>
-    //     <Snackbar
-    //       className="mb-5 lg:mb-5"
-    //       variant="success"
-    //       anchorOrigin={{ vertical: "top", horizontal: "center" }}
-    //       open={open}
-    //       onClose={handleClose}
-    //       message={DisplayItem()}
-    //       action={
-    //         <Button color="success" size="small" onClick={handleClose}>
-    //           {" "}
-    //           Close{" "}
-    //         </Button>
-    //       }
-    //     />
-    //   </div>
-    // </div>
-    // </ThemeProvider>
   );
 };
 
