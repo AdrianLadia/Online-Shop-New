3dc70b7954374e2b735441164b3647e8c18db286
AdrianLadia <ladia.adrian@gmail.com>
2023-10-11 15:30:07 +0800

    added download pdf option

--- online-shop/src/components/CartModal.jsx
+++ online-shop/src/components/CartModal.jsx
@@ -1,13 +1,14 @@
-import React, { useEffect } from "react";
-import { Modal, Typography } from "@material-ui/core";
-import Box from "@mui/material/Box";
-import { useContext, useState } from "react";
-import { FaShoppingCart } from "react-icons/fa";
-import useWindowDimensions from "./UseWindowDimensions";
-import CheckoutButton from "./CheckoutButton";
-import CartTable from "./CartTable";
-import Fade from "@mui/material/Fade";
-import AppContext from "../AppContext";
+import React, { useEffect } from 'react';
+import { Modal, Typography } from '@material-ui/core';
+import Box from '@mui/material/Box';
+import { useContext, useState } from 'react';
+import { FaShoppingCart } from 'react-icons/fa';
+import useWindowDimensions from './UseWindowDimensions';
+import CheckoutButton from './CheckoutButton';
+import CartTable from './CartTable';
+import Fade from '@mui/material/Fade';
+import AppContext from '../AppContext';
+import QuotationCreatorButton from './QuotationCreatorButton';
 
 const CartModal = (props) => {
   const openCart = props.openCart;
@@ -17,26 +18,22 @@ const CartModal = (props) => {
 
   const [cartisempty, setCartisempty] = useState(true);
   const { width, height } = useWindowDimensions();
-  const {
-    userdata,
-    firestore,
-    setCart,
-   } = React.useContext(AppContext);
+  const { userdata, firestore, setCart } = React.useContext(AppContext);
 
   const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    height: "80%",
-    transform: "translate(-50%, -50%)",
-    width: "95%",
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    height: '80%',
+    transform: 'translate(-50%, -50%)',
+    width: '95%',
 
-    "@media (min-width: 1024px)": {
-      width: "85%",
+    '@media (min-width: 1024px)': {
+      width: '85%',
     },
 
-    bgcolor: "background.paper",
-    border: "2px solid #000",
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
     boxShadow: 24,
     p: 4,
   };
@@ -57,34 +54,36 @@ const CartModal = (props) => {
 
   function clearCart() {
     setCart({});
-    firestore.createUserCart({cart:[]},userdata.uid)
-    }
+    firestore.createUserCart({ cart: [] }, userdata.uid);
+  }
 
   useEffect(() => {
     CheckIfCartIsEmpty();
+    console.log(finalCartData);
   }, [finalCartData]);
 
   return (
     <Modal open={openCart} onClose={CloseCart}>
-      <Fade in={openCart} >
+      <Fade in={openCart}>
         <Box sx={style} className="flex flex-col p-8 rounded-2xl overflow-y-auto bg-colorbackground border-color60">
-            <div className="flex flex-row-reverse mb-4">   
-              <button
-                id = 'closeCartButton'
-                onClick={CloseCart}
-                className="bg-red1 hover:bg-red-400 p-2 rounded-full w-10 text-white"
-              >
-                X
-              </button>
-            </div>
-          <div className="flex flex-row justify-between p-2">
-            <FaShoppingCart size={35}/>
-            <Typography
-              id="modal-modal-title"
-              variant="h6"
-              component="h2"
-              className="ml-auto font-bold"
+          <div className="flex flex-row justify-between mb-4">
+            <QuotationCreatorButton
+              arrayOfProductData={finalCartData}
+              deliveryFee={0}
+              companyName="Star Pack"
+              senderName=""
+            />
+            <button
+              id="closeCartButton"
+              onClick={CloseCart}
+              className="bg-red1 hover:bg-red-400 p-2 rounded-full w-10 text-white"
             >
+              X
+            </button>
+          </div>
+          <div className="flex flex-row justify-between p-2">
+            <FaShoppingCart size={35} />
+            <Typography id="modal-modal-title" variant="h6" component="h2" className="ml-auto font-bold">
               Cart Total: ₱ {totalPrice.toLocaleString()}
             </Typography>
           </div>
@@ -99,14 +98,13 @@ const CartModal = (props) => {
               <div className="flex flex-row justify-between w-full p-4">
                 <div>
                   <button
-                  id='clearCartButton'
+                    id="clearCartButton"
                     onClick={clearCart}
                     className="bg-red1h-10 mt-5 hover:bg-red-400 bg-red1 p-2 rounded-lg text-white "
                   >
                     Clear Cart
                   </button>
                 </div>
-
                 <div>
                   <CheckoutButton setOpenCart={setOpenCart} totalPrice={totalPrice} />
                 </div>
