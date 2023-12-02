ec984ef6e1ae548d84f70f9ca91313228ec39843
AdrianLadia <ladia.adrian@gmail.com>
2023-12-02 14:29:24 +0800

    .

--- online-shop/src/App.jsx
+++ online-shop/src/App.jsx
@@ -427,6 +427,7 @@ function App() {
         const keys = Object.keys(localStorageCart);
         const productDataPromises = keys.map(key => cloudfirestore.readSelectedDataFromOnlineStore(key));
         const newProductData = await Promise.all(productDataPromises);
+        
         setLocalCartProductsData(newProductData);
       }
       
