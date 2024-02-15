4d68e9e2d42646e2289511404a1b3dab6fe84bdc
AdrianLadia <ladia.adrian@gmail.com>
2024-02-15 20:08:30 +0800

    Create recoverFirestoreData.js

---
+++ online-shop/utils/classes/recoverFirestoreData.js
@@ -0,0 +1,36 @@
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
+import {
+  collection,
+  doc,
+  setDoc,
+  getDocs,
+  deleteDoc,
+  updateDoc,
+  getDoc,
+  arrayUnion,
+  arrayRemove,
+  runTransaction,
+  query,
+  where,
+  Timestamp
+} from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from '../../src/firebase_config.js';
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const ref = collection(db, 'Products')
+const timestamp = Timestamp.fromMillis(Date.parse('2024-02-13T12:31:00Z'))
+const documentSnapshot = await runTransaction(db,
+    updateFunction => updateFunction.get(ref),
+    {readOnly: true, readTime: timestamp}
+);
+
+const data = documentSnapshot.data();
+console.log(data);
+
+//   const querySnapshot = await firestore.runTransaction(
+//     updateFunction => updateFunction.get(query),
+//     {readOnly: true, readTime: new Firestore.Timestamp(1684098540, 0)}
+//   )
