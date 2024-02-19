0acad99d77d138faa97feb5597ad1a1fb5feab8c
AdrianLadia <ladia.adrian@gmail.com>
2024-02-20 07:20:07 +0800

    .

--- online-shop/utils/classes/recoverFirestoreData.js
+++ online-shop/utils/classes/recoverFirestoreData.js
@@ -21,14 +21,20 @@ import firebaseConfig from '../../src/firebase_config.js';
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const ref = collection(db, 'Products')
-const timestamp = Timestamp.fromMillis(Date.parse('2024-02-13T12:31:00Z'))
-const documentSnapshot = await runTransaction(db,
-    updateFunction => updateFunction.get(ref),
-    {readOnly: true, readTime: timestamp}
-);
+const snapshot = await getDocs(ref);
+snapshot.forEach((doc) => {
+  if (doc.data().unit != 'Pack') {
+    console.log(doc.id, '=>', doc.data().stocksAvailable);
+  }
+});
+// const timestamp = Timestamp.fromMillis(Date.parse('2024-02-13T12:31:00Z'))
+// const documentSnapshot = await runTransaction(db,
+//     updateFunction => updateFunction.get(ref),
+//     {readOnly: true, readTime: timestamp}
+// );
 
-const data = documentSnapshot.data();
-console.log(data);
+// const data = documentSnapshot.data();
+// console.log(data);
 
 //   const querySnapshot = await firestore.runTransaction(
 //     updateFunction => updateFunction.get(query),
