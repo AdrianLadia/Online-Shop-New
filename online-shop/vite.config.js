377efe62d1cc3e92eaa0ad412893a599501e8e89
Adrian Anton Ladia <ladia.adrian@gmail.com>
2024-03-18 13:34:28 +0800

    Revert "."
    This reverts commit 96b95f431a7a12bcfbf73e57f225b4c1ce9d6ca0.

--- online-shop/vite.config.js
+++ online-shop/vite.config.js
@@ -1,20 +1,15 @@
-import { defineConfig } from 'vite';
-import path from 'path';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    coverage: {
-      provider: 'c8',
-    },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-});
+   test: {
+      globals: true,
+      environment: 'jsdom',
+      coverage: {
+        provider: 'c8',
+      }
+    }
+})
