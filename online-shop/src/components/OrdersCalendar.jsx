c7edbb7fccfb87883d51b70a447fd977c035ab43
Adrian Anton Ladia <ladia.adrian@gmail.com>
2024-03-19 12:25:57 +0800

    .

--- online-shop/src/components/OrdersCalendar.jsx
+++ online-shop/src/components/OrdersCalendar.jsx
@@ -1,30 +1,31 @@
-import React from 'react'
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import { useState } from "react";
+import React from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { useState } from 'react';
 
 const OrdersCalendar = (props) => {
-    const startDate = props.startDate
-    const setStartDate = props.setStartDate
-    const disabledDates = props.disabledDates
-    const minDate = props.minDate
-    const maxDate = props.maxDate
-    const filterDate = props.filterDate
+  const startDate = props.startDate;
+  const setStartDate = props.setStartDate;
+  const disabledDates = props.disabledDates;
+  const minDate = props.minDate;
+  const maxDate = props.maxDate;
+  const filterDate = props.filterDate;
+  const width = props.width;
 
   return (
-    <div className='rounded-md'>
-       <DatePicker 
-          className='h-14 w-44 sm:w-56 rounded-md outline-color10b border-2 border-color10a indent-5 hover:border-color10b bg-white 
-                      placeholder:text-lg placeholder:text-color10b' 
-          placeholderText="Date"
-          selected={startDate}
-          onChange={(date) => setStartDate(date)}
-          minDate = {minDate}
-          maxDate = {maxDate}
-          filterDate = {filterDate}
-          />
+    <div className="rounded-md">
+      <DatePicker
+        className="h-12 w-full rounded-md outline-color10b border-2  indent-5 hover:border-color10b bg-slate-100
+                      placeholder:text-lg placeholder:text-color10b"
+        placeholderText="Delivery Date"
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        minDate={minDate}
+        maxDate={maxDate}
+        filterDate={filterDate}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default OrdersCalendar
+export default OrdersCalendar;
